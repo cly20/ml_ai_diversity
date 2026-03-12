7672e96bf91439a0d7db83de30f5d3e862e813e6
Eva Sabater <eva.sabateresa@gmail.com>
2026-03-12 16:53:32 +0000

    Add some headers for NN page

--- pages/3_nn.py
+++ pages/3_nn.py
@@ -5,9 +5,21 @@ st.set_page_config(layout="wide", page_title="NNs")
 
 st.title("Neural Networks")
 
+st.header("What is a Neural Network?")
+
+st.header("Brief History")
+
+st.header("Basic Structure")
+
+st.header("Interactive Example")
+
+st.header("Building a Neural Network with Python")
+
+st.header("Applications of Neural Networks")
+
 st.markdown("blah blah blah")
 
 if st.button("Next page"):
-  st.switch_page("pages/3_<InsertPageName>.py")
+  st.switch_page("pages/3_<InsertPageName>.py")  # CAHNGE
 if st.button("Previous page"):
-  st.switch_page("pages/1_Intro.py")
+  st.switch_page("pages/1_Intro.py")  # CHANGE
